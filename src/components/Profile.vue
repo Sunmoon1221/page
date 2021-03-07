cc70e30f66a046d9e94554a3bb99f434d391dffe
cyan <346029763@qq.com>
2021-03-07 18:05:48 +0800

    first commit

---
+++ src/components/Profile.vue
@@ -0,0 +1,365 @@
+<template>
+  <div class="profile">
+    <div class="header">
+      <img class="avatar" src="../assets/profile/avatar.png" alt="" />
+      <div class="user-info">
+        <span class="username">张三三</span>
+        <div class="info">
+          <span>福建厦门</span>
+          <span>15~20℃</span>
+        </div>
+      </div>
+    </div>
+    <div class="body">
+      <div class="feature">
+        <profile-item>
+          <template #img>
+            <img class="avatar" src="../assets/profile/shop.png" alt="" />
+          </template>
+          <template #text>
+            <span> 商城 </span>
+          </template>
+        </profile-item>
+        <profile-item>
+          <template #img>
+            <img class="avatar" src="../assets/profile/shop.png" alt="" />
+          </template>
+          <template #text>
+            <span> 商城 </span>
+          </template>
+        </profile-item>
+        <profile-item>
+          <template #img>
+            <img class="avatar" src="../assets/profile/shop.png" alt="" />
+          </template>
+          <template #text>
+            <span> 商城 </span>
+          </template>
+        </profile-item>
+        <profile-item>
+          <template #img>
+            <img class="avatar" src="../assets/profile/shop.png" alt="" />
+          </template>
+          <template #text>
+            <span> 商城 </span>
+          </template>
+        </profile-item>
+      </div>
+      <div class="ad">
+        <img src="../assets/profile/ad.png" alt="" />
+      </div>
+      <div class="all-app">
+        <p class="title">全部应用</p>
+        <div class="content">
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+        </div>
+      </div>
+      <div class="ai">
+        <p class="title">AI中心</p>
+        <div class="content">
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+          <profile-item class="item">
+            <template #img>
+              <img
+                class="avatar"
+                style="width: 40px; height: 42px"
+                src="../assets/profile/item.png"
+                alt=""
+              />
+            </template>
+            <template #text>
+              <span> 水产养殖 </span>
+            </template>
+          </profile-item>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileItem from "./ProfileItem";
+export default {
+  name: "Profile",
+  components: {
+    ProfileItem,
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+// 头部
+.header {
+	position: relative;
+  display: flex;
+  align-items: center;
+  height: 100px;
+  background-color: #dcebff;
+  background-clip: border-box;
+	&::after {
+		content: url('../assets/profile/setting.png');
+		display: block;
+		position: absolute;
+		right: 20px;
+		top: 10px;
+		width: 10px;
+		height: 20px;
+	}
+  .avatar {
+    margin:0 24px;
+  }
+
+  .user-info .info {
+    display: flex;
+    font-size: 8px;
+    color: #b5b5b5;
+
+    span {
+      border: 1px solid #b5b5b5;
+      border-radius: 10px;
+      padding: 2px;
+			margin-top: 4px;
+      margin-right: 6px;
+    }
+  }
+}
+// 主体
+.body {
+  padding: 0 24px;
+	height: calc(100vh - 100px);
+  background-color: #f3f3f3;
+}
+// 功能
+.feature {
+  display: flex;
+  padding: 10px;
+  justify-content: space-between;
+}
+// 广告
+.ad {
+  display: flex;
+  justify-content: center;
+  img {
+    width: 100%;
+  }
+}
+// 区域
+.all-app,
+.ai {
+  background-color: #fff;
+  margin: 10px 0 10px 0;
+	border-radius: 5px;
+	box-shadow: 0 0 2px rgb(224, 224, 224);;
+  .title {
+    font-size: 18px;
+    padding: 10px;
+  }
+}
+.all-app,
+.ai {
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+
+    .item {
+      width: 25%;
+      margin-bottom: 10px;
+    }
+  }
+}
+</style>
---
+++ src/components/ProfileItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="item">
+    <slot name="img">
+		</slot>
+    <slot name="text"> </slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+ .item {
+	 display: flex;
+	 flex-direction: column;
+	//  justify-content: center;
+	align-items: center;
+
+	 img {
+		 height: 30px;
+		 width: 40px;
+		 margin-bottom: 4px;
+	 }
+ }
+</style>
